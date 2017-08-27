d5459bdccd181283fa1204cbf96c5c0e16be66d9
ZipCPU <dgisselq@ieee.org>
2017-08-26 21:02:34 -0400

    Added more comments to the devbus.h interface definition file

--- sw/devbus.h
+++ sw/devbus.h
@@ -15,7 +15,7 @@
 //
 ////////////////////////////////////////////////////////////////////////////////
 //
-// Copyright (C) 2015-2016, Gisselquist Technology, LLC
+// Copyright (C) 2015-2017, Gisselquist Technology, LLC
 //
 // This program is free software (firmware): you can redistribute it and/or
 // modify it under the terms of  the GNU General Public License as published
@@ -28,7 +28,7 @@
 // for more details.
 //
 // You should have received a copy of the GNU General Public License along
-// with this program.  (It's in the $(ROOT)/doc directory, run make with no
+// with this program.  (It's in the $(ROOT)/doc directory.  Run make with no
 // target there if the PDF file isn't present.)  If not, see
 // <http://www.gnu.org/licenses/> for a copy.
 //
@@ -61,18 +61,56 @@ public:
 	virtual	void	close(void) = 0;
 
 	// Write a single value to a single address
+	//	a is the address of the value to be read as it exists on the
+	//		wishbone bus within the FPGA.
+	//	v is the singular value to write to this address
 	virtual	void	writeio(const BUSW a, const BUSW v) = 0;
 
 	// Read a single value to a single address
+	//	a is the address of the value to be read as it exists on the
+	//		wishbone bus within the FPGA.
+	//	This function returns the value read from the device wishbone
+	//		at address a.
 	virtual	BUSW	readio(const BUSW a) = 0;
 
 	// Read a series of values from values from a block of memory
+	//	a is the address of the value to be read as it exists on the
+	//		wishbone bus within the FPGA.
+	//	len is the number of words to read
+	//	buf is a pointer to a place to store the words once read.
+	// This is equivalent to:
+	//	for(int i=0; i<len; i++)
+	//		buf[i] = readio(a+i);
+	// only it's faster in our implementation.
 	virtual	void	readi(const BUSW a, const int len, BUSW *buf) = 0;
 
-	// Read a series of values from the same address in memory
+	// Read a series of values from the same address in memory.  This
+	// call is identical to readi, save that the address is not incremented
+	// from one read to the next.  It is equivalent to:
+	//	for(int i=0; i<len; i++)
+	//		buf[i] = readio(a);
+	// only it's faster in our implementation.
+	//
 	virtual	void	readz(const BUSW a, const int len, BUSW *buf) = 0;
 
+	// Write a series of values into a block of memory on the FPGA
+	//	a is the address of the value to be written as it exists on the
+	//		wishbone bus within the FPGA.
+	//	len is the number of words to write
+	//	buf is a pointer to a place to from whence to grab the data
+	//		to be written.
+	// This is equivalent to:
+	//	for(int i=0; i<len; i++)
+	//		writeio(a+i, buf[i]);
+	// only it's faster in our implementation.
 	virtual	void	writei(const BUSW a, const int len, const BUSW *buf) = 0;
+	// Write a series of values into the same address on the FPGA bus.  This
+	// call is identical to writei, save that the address is not incremented
+	// from one write to the next.  It is equivalent to:
+	//	for(int i=0; i<len; i++)
+	//		writeio(a, buf[i]);
+	// only it's faster in our implementation.
+	//
 	virtual	void	writez(const BUSW a, const int len, const BUSW *buf) = 0;
 
 	// Query whether or not an interrupt has taken place
